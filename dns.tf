f2c67653b72b1ee187cc70f5bc549f079656e407
Zoellner122 <r1997@live.nl>
2023-04-11 15:50:12 +0200

    Adding domains to be managed by DO

---
+++ dns.tf
@@ -0,0 +1,7 @@
+resource "digitalocean_domain" "romanoffindustries" {
+  name = "romanoffindustries.nl"
+}
+
+resource "digitalocean_domain" "ri-cs" {
+  name = "ri-cs.nl"
+}
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source  = "digitalocean/digitalocean"
+      source = "digitalocean/digitalocean"
     }
   }
 
@@ -9,12 +9,22 @@ terraform {
     organization = "ri-cs"
     workspaces {
       name = "ri-cs"
-    } 
+    }
   }
 }
 
 resource "digitalocean_project" "ri-cs" {
-    name        = "ri-cs"
-    description = "The default Ri-CS project"
-    environment = "Production"
+  name       = "ri-cs"
+  is_default = true
+
+  purpose     = "Other"
+  description = "The default Ri-CS project"
+  environment = "Production"
+}
+
+resource "digitalocean_vpc" "ri-cs" {
+  name   = "ri-cs"
+  region = "ams3"
+
+  ip_range = "10.100.0.0/20"
 }
