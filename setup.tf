15607054ecfde777297f7b37182c628a65ea7df1
Zoellner122 <r1997@live.nl>
2023-04-12 10:37:06 +0200

    Setting default resource region

--- setup.tf
+++ setup.tf
@@ -1,3 +1,9 @@
 provider "digitalocean" {
   token = var.do_token
 }
+
+variable "do_region" {
+  description = "Region where a resource (e.g. Droplet) should land in"
+  default     = "ams3"
+  type        = string
+}
